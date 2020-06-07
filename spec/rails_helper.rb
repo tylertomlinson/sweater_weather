554e02756ca2ae6fb9340f53449a4e27ed3d6af3
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-06-06 23:21:49 -0600

    Add weather service test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -44,7 +44,8 @@ Shoulda::Matchers.configure do |config|
 end
 
 VCR.configure do |config|
-  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  # config.allow_http_connections_when_no_cassette = true
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
   config.configure_rspec_metadata!
   config.filter_sensitive_data('<GOOGLE_GEO_API>') { ENV['GOOGLE_GEO_API'] }
---
+++ spec/services/weather_service_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe WeatherService, :vcr do
+  it 'exists' do
+    service = WeatherService.new
+
+    expect(service).to be_an_instance_of(WeatherService)
+  end
+
+  it 'can get data on weather by location' do
+    coordinates = { lat: 30.3321838, lon: -81.65565099999999 }
+    weather = WeatherService.weather_data(coordinates[:lat], coordinates[:lon])
+
+    expect(weather).to be_an_instance_of(Hash)
+    expect(weather[:current]).to have_key(:sunrise)
+    expect(weather[:current]).to have_key(:sunset)
+    expect(weather[:current]).to have_key(:feels_like)
+  end
+end
