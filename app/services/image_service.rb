65b97c62b7869867f861500ebc613acc8cf95b14
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-06-08 16:14:57 -0600

    Update image service

--- app/services/image_service.rb
+++ app/services/image_service.rb
@@ -1,2 +1,16 @@
 class ImageService
-end 
+
+def get_background_image(location)
+  city = location.split(',').first
+  response = conn.get('/search/photos') do |f|
+    f.params[:query] = city
+    f.params[:client_id] = ENV['UNSPLASH_API']
+end
+  JSON.parse(response.body, symbolize_names: true)[:results].first[:urls][:raw]
+end
+
+private
+  def conn
+    Faraday.new('https://api.unsplash.com')
+  end
+end
