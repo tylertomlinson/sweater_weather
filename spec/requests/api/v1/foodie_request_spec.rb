708fe4355842866a16ca08342a1f6050e8384d8a
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-06-08 11:59:09 -0600

    All tests passing

--- spec/requests/api/v1/foodie_request_spec.rb
+++ spec/requests/api/v1/foodie_request_spec.rb
@@ -3,8 +3,11 @@ require 'rails_helper'
 describe 'foodie request API' do
   it 'can return forecast data' do
     get '/api/v1/foodie?start=denver,co&end=pueblo,co&search=italian'
-    data = JSON.parse(response.body, symbolize_names: true)
+    json = JSON.parse(response.body, symbolize_names: true)
     expect(response).to be_successful
-    expect(data)
+    expect(json[:data][:attributes]).to have_key(:forecast)
+    expect(json[:data][:attributes]).to have_key(:temp)
+    expect(json[:data][:attributes]).to have_key(:travel_time)
+    expect(json[:data][:attributes]).to have_key(:end_location)
   end
 end
