747aba419190f4916236c6944fec3dbb71ba5c3d
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-06-08 00:04:30 -0600

    Add index action with json render

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,3 +1,13 @@
-class ForecastController < ApplicationController
-  def index; end 
+class Api::V1::ForecastController < ApplicationController
+  def index
+    coords = get_the_coords(params[:location])
+    weather_data = WeatherService.new.get_weather_data(coords[:lat], coords[:lng])
+    render json: ForecastSerializer.new(Forecast.new(weather_data))
+  end
+end
+
+private
+
+def get_the_coords(location)
+  GeoCodeService.new.get_coords(params[:location])[:geometry][:location]
 end
