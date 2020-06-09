945801dcb5d8439ee8673a4c79ea571a37f72e37
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-06-09 13:34:32 -0600

    Rubocop

--- spec/requests/api/v1/user_registration_resquest_spec.rb
+++ spec/requests/api/v1/user_registration_resquest_spec.rb
@@ -2,13 +2,15 @@ require 'rails_helper'
 
 describe 'User request API' do
   it 'can create user' do
-    user_info = { email: 'test@example.com',
-                  password: 'test',
-                  password_confirmation: 'test' }
+    user_info = {
+      email: 'test@example.com',
+      password: 'test',
+      password_confirmation: 'test'
+    }
 
-    post '/api/v1/users',
-    params: user_info.to_json,
-    headers: { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }
+    post  '/api/v1/users',
+          params: user_info.to_json,
+          headers: { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }
 
     expect(response).to be_successful
     expect(response.status).to eq(201)
@@ -25,13 +27,15 @@ describe 'User request API' do
 
   it 'will return status 400 with error msg if email is duplicate' do
     user1 = create(:user)
-    user2_info = { email: user1.email,
-                  password: 'test',
-                  password_confirmation: 'test' }
+    user2_info = {
+      email: user1.email,
+      password: 'test',
+      password_confirmation: 'test'
+    }
 
-    post '/api/v1/users',
-    params: user2_info.to_json,
-    headers: { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }
+    post  '/api/v1/users',
+          params: user2_info.to_json,
+          headers: { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }
 
     expect(response.status).to eq(400)
 
@@ -42,13 +46,15 @@ describe 'User request API' do
   end
 
   it 'will return status 400 with error msg if passwords do not match' do
-    user_info = { email: 'test@example.com',
-                  password: 'test',
-                  password_confirmation: 'test1' }
+    user_info = {
+      email: 'test@example.com',
+      password: 'test',
+      password_confirmation: 'test1'
+    }
 
-    post '/api/v1/users',
-    params: user_info.to_json,
-    headers: { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }
+    post  '/api/v1/users',
+          params: user_info.to_json,
+          headers: { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }
 
     expect(response.status).to eq(400)
 
@@ -59,13 +65,15 @@ describe 'User request API' do
   end
 
   it 'will return status 404 with error msg if passwords do not match (case sensitive)' do
-    user_info = { email: 'test@example.com',
-                  password: 'tEST',
-                  password_confirmation: 'test' }
+    user_info = {
+      email: 'test@example.com',
+      password: 'tEST',
+      password_confirmation: 'test'
+    }
 
-    post '/api/v1/users',
-    params: user_info.to_json,
-    headers: { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }
+    post  '/api/v1/users',
+          params: user_info.to_json,
+          headers: { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }
 
     expect(response.status).to eq(400)
 
@@ -75,20 +83,41 @@ describe 'User request API' do
     expect(json_response[:status]).to eq(400)
   end
 
-  it 'will return status 404 with error msg if all fields are not filled in' do
-    user_info = { email: '',
-                  password: 'test',
-                  password_confirmation: 'test' }
+  it 'will return status 404 with error msg if all fields are not filled in (email)' do
+    user_info = {
+      email: '',
+      password: 'test',
+      password_confirmation: 'test'
+    }
 
-    post '/api/v1/users',
-    params: user_info.to_json,
-    headers: { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }
+    post  '/api/v1/users',
+          params: user_info.to_json,
+          headers: { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }
 
     expect(response.status).to eq(400)
 
     json_response = JSON.parse(response.body, symbolize_names: true)
 
-    expect(json_response[:body]).to eq("Missing required fields")
+    expect(json_response[:body]).to eq("Email can't be blank")
+    expect(json_response[:status]).to eq(400)
+  end
+
+  it 'will return status 404 with error msg if all fields are not filled in (password)' do
+    user_info = {
+      email: 'test@example.com',
+      password: '',
+      password_confirmation: 'test'
+    }
+
+    post  '/api/v1/users',
+          params: user_info.to_json,
+          headers: { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }
+
+    expect(response.status).to eq(400)
+
+    json_response = JSON.parse(response.body, symbolize_names: true)
+
+    expect(json_response[:body]).to eq("Password digest can't be blank and Password can't be blank")
     expect(json_response[:status]).to eq(400)
   end
 end
