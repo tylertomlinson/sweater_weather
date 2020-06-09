2c7f295e150b1492855841f2324dcd6662fb0ace
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-06-08 21:07:05 -0600

    User model with test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: true
+  validates :api_key, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, confirmation: { case_sensitive: true }
+  has_secure_password
+  has_secure_token :api_key
 end
