6cbdd6db2c010e76f0400eb76b3e28480a77d7f5
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-06-08 22:48:40 -0600

    Returning status 200

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,3 +1,22 @@
 class Api::V1::UsersController < ApplicationController
-  def create; end 
+  def create
+    user = User.new(user_params)
+
+    user.save ? success(user) : failure(user)
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
+  end
+
+  def success(user)
+    render json: { status: 201, body: UserSerializer.new(user) }
+  end
+
+  def failure(user)
+    render json: { status: 400, body: user.errors.full_messages.to_sentence}
+  end
+
 end
