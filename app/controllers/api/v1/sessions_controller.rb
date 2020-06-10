a8b03138d3b52c3170fe9dba86fc12b6b54e4cf0
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-06-09 18:49:21 -0600

    User login

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,3 +1,17 @@
 class Api::V1::SessionsController < ApplicationController
-  def create; end
+  def create
+    user = User.find_by(email: params[:email])
+    user && user.authenticate(params[:password]) ? success(user) : failure
+  end
+
+private
+
+  def success(user)
+    render json: UserSerializer.new(user), status: 200
+  end
+
+  def failure
+    msg = { body: 'The credentials entered are invalid', status: 400 }
+    render json: msg, status: :bad_request
+  end
 end
