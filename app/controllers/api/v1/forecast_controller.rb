218d458cd43033e8a41230796af0bee73159a568
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-06-09 19:17:37 -0600

    Refactor to private methods

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,7 +1,15 @@
 class Api::V1::ForecastController < Api::V1::BaseController
   def index
-    coords = the_coords(params[:location])
-    weather_data = WeatherService.new.get_weather_data(coords[:lat], coords[:lng])
     render json: ForecastSerializer.new(Forecast.new(weather_data))
+  end
+
+  private
+
+  def coords
+    the_coords(params[:location])
+  end
+
+  def weather_data
+    WeatherService.new.get_weather_data(coords[:lat], coords[:lng])
   end
 end
